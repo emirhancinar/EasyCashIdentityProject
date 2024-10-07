2218d4fda583f76ff5050ed59399635aadcffeff
Emirhan Çınar <emirhancinar025@gmail.com>
2024-10-07 14:24:01 +0300

    Adding entity conrete and first migrations

---
+++ EasyCashIdentityProject.DataAccessLayer/Concrete/Context.cs
@@ -0,0 +1,23 @@
+﻿using EasyCashIdentityProject.EntityLayer.Concete;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyCashIdentityProject.DataAccessLayer.Concrete
+{
+    public class Context : IdentityDbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=DESKTOP-2I8FV16\\SQLEXPRESS;Database=EasyCashDb;User Id=sa;Password=sapass;MultipleActiveResultSets=true;TrustServerCertificate=True;");
+        }
+        public DbSet<CustomerAccount> CustomerAccounts { get; set; }
+        public DbSet<CustomerAccountProcess> CustomerAccountProcesses { get; set; }
+
+
+    }
+}
---
+++ EasyCashIdentityProject.EntityLayer/Concete/CustomerAccountProcess.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyCashIdentityProject.EntityLayer.Concete
+{
+    public class CustomerAccountProcess
+    {
+        public int CustomerAccountProcessID { get; set; }
+        public string ProcessType { get; set; }
+        public decimal Amount { get; set; }  //Miktar
+        public DateTime ProcessDate { get; set; }
+
+    }
+}
+ 
