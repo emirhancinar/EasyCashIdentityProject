6971d95ea638b0d9ac0b62e153ebb0dcabd977da
Emirhan Çınar <emirhancinar025@gmail.com>
2024-10-07 16:06:21 +0300

    Customizing AspNet User and AspNet Role Tables

---
+++ EasyCashIdentityProject.EntityLayer/Concete/AppUser.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyCashIdentityProject.EntityLayer.Concete
+{
+    public class AppUser:IdentityUser<int>
+    {
+        public string Name { get; set; }
+        public string SurName { get; set; }
+        public string District { get; set; } // İlçe ya da Semt
+        public string City { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
